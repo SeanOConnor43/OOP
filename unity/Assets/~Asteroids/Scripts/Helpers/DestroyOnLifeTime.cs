03022647ceaba763a1a4d7b382e73c6bae95bbe1
sean.oconnor33 <sean.oconnor33@600900B211DE415>
2018-05-02 12:02:53 +1000

    Changed file locations and started visual studio scripts.

---
+++ unity/Assets/~Asteroids/Scripts/Helpers/DestroyOnLifeTime.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroids
+{
+    public class DestroyOnLifeTime : MonoBehaviour
+    {
+        public float lifeTime = 5f;
+        // Use this for initialization
+        void Start()
+        {
+            Destroy(gameObject, lifeTime);
+        }
+    }
+}
---
+++ visualstudio/HelloWorld/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Color
+    {
+        public float r, g, b;
+    }
+    class Dog
+    {
+        public string name;
+        public int size;
+        public string breed;
+        public Color color;
+
+        public void Speak()
+        {
+            Console.WriteLine(name + " says: Woof!");
+        }
+        public void Walk()
+        {
+            Console.WriteLine(name + " is walking.");
+        }
+        public void Eat(string food)
+        {
+            Console.WriteLine(name + " is eating " + food + ".");
+        }
+        public void Shit()
+        {
+            Console.WriteLine(name + " is shitting.");
+        }
+        public void Sleep()
+        {
+            Console.WriteLine(name + " is sleeping.");
+        }
+
+    }
+    class Program
+    {
+        public static string help = @"Welcome to shitty dog Simulator
+        ";
+        static void Main(string[] args)
+        {
+            
+            bool quit = false;
+            string inThing;
+            while (quit == false)
+            {
+               
+            }
+            
+        }
+    }
+}
